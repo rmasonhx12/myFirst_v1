7fc09d8a35ee1232034d813f56e473c7bc2d6e90
Robert Hendricks <rmasonh22@gmail.com>
2024-08-18 02:27:29 -0400

    Initial Commit

---
+++ myFirst_v1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  myFirst_v1
+//
+//  Created by Robert Hendricks on 8/18/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ myFirst_v1/myFirst_v1App.swift
@@ -0,0 +1,17 @@
+//
+//  myFirst_v1App.swift
+//  myFirst_v1
+//
+//  Created by Robert Hendricks on 8/18/24.
+//
+
+import SwiftUI
+
+@main
+struct myFirst_v1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
