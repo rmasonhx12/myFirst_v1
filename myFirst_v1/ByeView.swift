544db3848c40ba58135f47690d1feca16e61561e
Robert Hendricks <rmasonh22@gmail.com>
2024-08-18 02:29:06 -0400

    initial app

---
+++ myFirst_v1/ByeView.swift
@@ -0,0 +1,18 @@
+//
+//  ByeView.swift
+//  myFirst_v1
+//
+//  Created by Robert Hendricks on 8/18/24.
+//
+
+import SwiftUI
+
+struct ByeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ByeView()
+}
